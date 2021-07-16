b2ffdebd6255866e00a061af3054b40d64253cf1
= <=>
2021-07-16 00:14:58 -0400

    se genero el componente de los albums, su servicio y su modelo y en el  componente se hizo la prueba de un simple input

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { AlbumsComponent } from './components/albums/albums.component';
+const routes: Routes = [
+  {
+    path: "",
+    component: AlbumsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/albums/albums.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Album } from 'src/app/models/album.model';
+import { AlbumService } from 'src/app/services/album.service';
+
+@Component({
+  selector: 'app-albums',
+  templateUrl: './albums.component.html',
+  styleUrls: ['./albums.component.css']
+})
+export class AlbumsComponent implements OnInit {
+
+  albums: Album[]=[];
+  nameFiltered = "";
+
+  constructor(
+    public router: Router,
+    private route: ActivatedRoute,
+    public albumService: AlbumService
+  ) { }
+
+  ngOnInit(): void {
+    
+  }
+
+  searchAuthos()
+  {
+    console.log(this.nameFiltered);
+  }
+}
